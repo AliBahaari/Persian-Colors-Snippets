813c43044a8be6a63867a8fc428adfef7bbcab93
Ali <alibahaari1997@gmail.com>
2023-07-15 01:06:19 +0330

    JavaScript + Python + TypeScript

---
+++ PersianColors.js
@@ -0,0 +1,11 @@
+const PersianColors = {
+  PersianBlue: "#1C39BB",
+  PersianMediumBlue: "#0067A5",
+  PersianIndigo: "#32127A",
+  PersianRose: "#FE28A2",
+  PersianPink: "#F77FBE",
+  PersianGreen: "#00A693",
+  PersianPlum: "#701C1C",
+  PersianRed: "#CC3333",
+  PersianOrange: "#D99058",
+};
---
+++ PersianColors.ts
@@ -0,0 +1,15 @@
+type PersianColorsType = {
+  [key: string]: string;
+};
+
+const PersianColors: PersianColorsType = {
+  PersianBlue: "#1C39BB",
+  PersianMediumBlue: "#0067A5",
+  PersianIndigo: "#32127A",
+  PersianRose: "#FE28A2",
+  PersianPink: "#F77FBE",
+  PersianGreen: "#00A693",
+  PersianPlum: "#701C1C",
+  PersianRed: "#CC3333",
+  PersianOrange: "#D99058",
+};
